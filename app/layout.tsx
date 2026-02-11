1a9f01a7f16dad2f27937f9ca9392727a74dc8bb
MariaV <maariv@example.com>
2026-02-11 18:54:20 +0200

    Add Google verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ro">
       <head>
-        {/* Google site verification */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Calculator Fiscal România 2026</title>
+
+        {/* META GOOGLE VERIFICATION */}
         <meta
           name="google-site-verification"
           content="Qdw-Q-15s8y0fkQKwn-BSmTCRgEyJMh8a1xm6LKA2GU"
         />
-        <title>Calculator Fiscal România 2026</title>
       </head>
       <body>{children}</body>
     </html>
